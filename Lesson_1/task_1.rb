d10b2d32aaccbca9c9a43c33b40ad57b674b42a0
Arcadnii <golubevaleksej73@gmail.com>
2022-04-12 17:37:56 +0400

    L_1

---
+++ Lesson_1/task_1.rb
@@ -0,0 +1,13 @@
+puts "Как вас зовут?"
+name = gets.chomp
+
+puts "Какой у вас рост(см)?"
+height = gets.chomp
+
+weight = (height.to_i-110)*1.15
+
+if  weight > 0 
+	puts "#{name}, ваш идеальный вес #{weight} кг."
+elsif	weight < 0 
+	puts "Ваш вес уже оптимальный."
+	end
---
+++ Lesson_1/task_4.rb
@@ -0,0 +1,17 @@
+puts "Введите коэффициент a."
+a = gets.chomp.to_i
+puts "Введите коэффициент b."
+b = gets.chomp.to_i
+puts "Введите коэффициент c."
+c = gets.chomp.to_i
+d = b**2 - 4*a*c
+if d>0
+	x1 = (-b+Math.sqrt(d)/2*a)
+	x2 = (-b-Math.sqrt(d)/2*a)
+	puts "Дискриминант = #{d}.  Корень X1 = #{x1}; X2 = #{x2}"
+elsif d == 0
+	x1 = (-b+Math.sqrt(d)/2*a)
+	puts "Дискриминант = #{d}.  Корень X = #{x1}"
+elsif d < 0
+	puts "Корней нет."
+end
